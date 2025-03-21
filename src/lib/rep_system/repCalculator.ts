085789f9f219828b47cc323863b99f1d01b369c6
jstEagle <hunekejustus@gmail.com>
2025-03-22 10:43:39 +1300

    val and power

---
+++ src/lib/rep_system/repCalculator.ts
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @param C constant C, defaults to 2
+ * @returns A value calculator that returns the value for the initialised C given any n
+ */
+function val(C: number = 2): (n: number) => number {
+    return function(n: number) {
+        // Calculates value given constant C and number of times traded in last 30 days (n)
+        return C * Math.max(-1*Math.log10(n + 0.5) + 1, 0.05);
+    };
+}
+
+/**
+ * 
+ * @param val value of the interaction for the trader
+ * @param rep reputation of the trader
+ * @param boost boost (defaults to 1)
+ * @returns 
+ */
+const power = (val: number, rep: number, boost: number = 1) => {
+    return val * rep * boost;
+}

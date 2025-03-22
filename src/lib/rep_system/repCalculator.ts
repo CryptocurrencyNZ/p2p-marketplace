1ab96f9c71bde76957d4b8480caaf9f4507021d8
jstEagle <hunekejustus@gmail.com>
2025-03-22 14:48:12 +1300

    final formula

--- src/lib/rep_system/repCalculator.ts
+++ src/lib/rep_system/repCalculator.ts
@@ -1,49 +1,20 @@
-/**
- * 
- * @param C constant C, defaults to 2
- * @returns A value calculator that returns the value for the initialised C given any n
- */
-function valCalc(C: number = 2): (n: number) => number {
-    return function(n: number) {
-        // Calculates value given constant C and number of times traded in last 30 days (n)
-        return C * Math.max(-1*Math.log10(n + 0.5) + 1, 0.05);
-    };
+
+const expected = (rA: number, rB: number, d?: number): number => {
+    return 1 / (1 + Math.pow(10, (rA - rB) / (d ?? 1000)));
 }
 
-/**
- * 
- * @param val value of the interaction for the trader
- * @param rep reputation of the trader
- * @param boost boost (defaults to 1)
- * @returns 
- */
-const power = (n: number, rep: number, C?: number): number => {
-    const val = valCalc(C);
-    return (val(n) * rep) / 2000 ;
+const weight = (rA: number, c?: number): number => {
+    return rA / (rA + (c ?? 5000));
 }
 
-const prob = (repA: number, repB: number) => {
-    return 1 / (1 + (Math.pow(10, (repB - repA) / 400)));
+function repUpdate(rReciever: number, rRater: number, score: number, numTrades: number, k?: number): number {
+    if (score === 0) {
+        return rReciever + 1;
+    }
+    let newRating = rReciever + ((k ?? 150) * (1 / (0.6 * Math.sqrt(numTrades + 1)))) * weight(rRater) * (score - expected(rRater, rReciever));
+    newRating = Math.min(newRating, 2000);
+    newRating = Math.round(newRating);
+    return newRating;
 }
 
-const nextRep = (repA: number, repB: number, scoreA: number, K: number = 10): number => {
-    return repA + K * (scoreA - prob(repA, repB));
-}
-
-function completeInteraction(repA: number, repB: number, scoreA: number, scoreB: number, n: number, K?: number, C?: number): [number, number] {
-    const powerA = power(n, repA);
-    const powerB = power(n, repB);
-
-    console.log(`Rep A before: ${repA}`);
-    console.log(`Rep B before: ${repB}`);
-
-    repA = scoreA * nextRep(repA, repB, scoreA, powerB);
-    repB = scoreB * nextRep(repB, repA, scoreB, powerA);
-
-    console.log(`Rep A after: ${repA}`);
-    console.log(`Rep B after: ${repB}`);
-
-    return [repA, repB];
-}
-
-completeInteraction(1100, 1000, 1.1, 0.9, 2);
+console.log(repUpdate(1500, 2000, 1, 2));

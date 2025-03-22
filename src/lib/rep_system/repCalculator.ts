3e03bad4127b872a41b06f7afe88031c4ad51eb5
jstEagle <hunekejustus@gmail.com>
2025-03-22 15:15:24 +1300

    final

--- src/lib/rep_system/repCalculator.ts
+++ src/lib/rep_system/repCalculator.ts
@@ -1,17 +1,17 @@
 
-const expected = (rA: number, rB: number, d?: number): number => {
-    return 1 / (1 + Math.pow(10, (rA - rB) / (d ?? 1000)));
+const expected = (rA: number, rB: number): number => {
+    return 1 / (1 + Math.pow(10, (rA - rB) / 1000));
 }
 
-const weight = (rA: number, c?: number): number => {
-    return rA / (rA + (c ?? 5000));
+const weight = (rA: number): number => {
+    return rA / (rA + 5000);
 }
 
-function repUpdate(rReciever: number, rRater: number, score: number, numTrades: number, k?: number): number {
+function repUpdate(rReciever: number, rRater: number, score: number, numTrades: number): number {
     if (score === 0) {
         return rReciever + 1;
     }
-    let newRating = rReciever + ((k ?? 150) * (1 / (0.6 * Math.sqrt(numTrades + 1)))) * weight(rRater) * (score - expected(rRater, rReciever));
+    let newRating = rReciever + (150 * (1 / (0.6 * Math.sqrt(numTrades + 1)))) * weight(rRater) * (score - expected(rRater, rReciever));
     newRating = Math.min(newRating, 2000);
     newRating = Math.round(newRating);
     return newRating;

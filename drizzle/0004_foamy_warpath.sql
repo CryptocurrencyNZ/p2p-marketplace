00581025cf3bd10ddcc23c82b352f032ca8efe8b
j7cb1 <jacobb7331@gmail.com>
2025-03-22 16:06:46 +1300

    Nuked drizzle folder

--- drizzle/0004_foamy_warpath.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "userProfiles" DROP CONSTRAINT "userProfiles_id_unique";--> statement-breakpoint
-ALTER TABLE "userProfiles" ADD PRIMARY KEY ("id");--> statement-breakpoint
-ALTER TABLE "userProfiles" ALTER COLUMN "id" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "userProfiles" ADD COLUMN "auth_id" text;--> statement-breakpoint
-ALTER TABLE "userProfiles" ADD CONSTRAINT "userProfiles_auth_id_unique" UNIQUE("auth_id");
--- drizzle/0005_petite_rockslide.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "userProfiles" ADD COLUMN "age" integer;

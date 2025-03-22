f2e5e5bd2168e75155f06a3b554efda098745dc5
0xjimmy <me@0xjimmy.xyz>
2025-03-22 20:35:21 +1300

    export db

--- src/db.ts
+++ src/db.ts
@@ -2,11 +2,5 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const queryClient = postgres(process.env.DATABASE_URL);
-const db = drizzle({ client: queryClient });
-
-const result = await db.execute("select 1");
-
-// import { drizzle } from "drizzle-orm/node-postgres";
-
-// export const db = drizzle(process.env.DATABASE_URL!);
+const queryClient = postgres(process.env.DATABASE_URL!);
+export const db = drizzle({ client: queryClient });
